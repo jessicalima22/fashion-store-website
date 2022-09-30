c9dadeb20cbd572f6db958e31d2fda9bd7c5af9b
jessicalima22 <jessica.lima22@hotmail.com>
2022-09-30 01:04:20 -0300

    ready page for desktop
    Finished page for desktop

--- JS/script.js
+++ JS/script.js
@@ -13,7 +13,7 @@ $('#submit').mouseenter(function(){
 
 $('#submit').mouseleave(function(){
     $(this).css({
-        'background': ' url(images/icon-arrow.svg), linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))',
+        'background': 'url(images/icon-arrow.svg), linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))',
         'background-repeat': 'no-repeat',
         'background-position': 'center',
         'box-shadow': '0px 10px 40px  hsl(0, 35%, 78%)'
@@ -21,10 +21,11 @@ $('#submit').mouseleave(function(){
 });   
 });
 
-const email = document.getElementById('email');
+const email = document.querySelector('input');
 const submit = document.getElementById('submit');
 const form = document.getElementById('form');
-const emailValue = email.value
+
+
 
 
 form.addEventListener('submit', (e) => {
@@ -34,17 +35,37 @@ form.addEventListener('submit', (e) => {
 
 
 function checkInputs(){
+    const emailValue = email.value;
     if (emailValue === '') {
-        console.log('testinho');
-
+        
+        setErrorFor(email, "Can't be blank");
+    } else if (!checkEmail(emailValue)){
+        setErrorFor(email, 'Please provide a valid email');
+    } else {
         const small = document.querySelector('small');
         const iconError = document.getElementById('error');
-        console.log(iconError)
         
-        small.style.display = 'inline';
-        email.style.border = '2px solid hsl(0, 93%, 68%)';
-        iconError.style.display = 'inline';
+        small.style.display = 'none';
+        email.style.border = 'solid 0.5px hsla(0, 36%, 70%, 0.5)';
+        iconError.style.display = 'none';
+    }
 
- }};
+};
 
+
+ function setErrorFor (email, message){
+    const small = document.querySelector('small');
+    const iconError = document.getElementById('error');
+    
+    small.innerText = message;
+    small.style.display = 'inline';
+    email.style.border = '2px solid hsl(0, 93%, 68%)';
+    iconError.style.display = 'inline';
+ }
+
+ function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
      
