19df372ad7766331d968b4d8ca6477bc649ef006
jessicalima22 <jessica.lima22@hotmail.com>
2022-09-29 23:37:09 -0300

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,50 @@
+    
+$(document).ready(function() {
+
+$('#submit').mouseenter(function(){
+    $(this).css({
+        'background': 'url(images/icon-arrow.svg), linear-gradient(135deg, hsl(358, 67%, 92%), hsl(355, 67%, 85%))',
+        'background-repeat': 'no-repeat',
+        'background-position': 'center',
+        'box-shadow': '0px 15px 20px  hsl(0, 35%, 78%)'
+    }); 
+    $(this).css ('cursor', 'pointer');
+});
+
+$('#submit').mouseleave(function(){
+    $(this).css({
+        'background': ' url(images/icon-arrow.svg), linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))',
+        'background-repeat': 'no-repeat',
+        'background-position': 'center',
+        'box-shadow': '0px 10px 40px  hsl(0, 35%, 78%)'
+    }); 
+});   
+});
+
+const email = document.getElementById('email');
+const submit = document.getElementById('submit');
+const form = document.getElementById('form');
+const emailValue = email.value
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+ });
+
+    
+
+     
+    
+
+function checkInputs(){
+    if (emailValue === '') {
+        console.log('testinho');
+
+        const small = document.querySelector('small');
+        console.log(small);
+        small.style.display = 'inline';
+
+ }};
+
+     
